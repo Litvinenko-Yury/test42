5df61930e1d4f52ae6e0dfb8b942a48976269448
Литвиненко Юрий <litvinenko-don@ukr.net>
2021-03-25 14:06:28 +0300

    js: added slider-carousel.js

--- source/js/modules/slider-carousel.js
+++ source/js/modules/slider-carousel.js
@@ -5,8 +5,7 @@ function sliderCarousel() {
   //найти все слайдеры
   const sliderCarouselAll = document.querySelectorAll('.slider-carousel');
 
-  setSliderFunctionality();
-
+  setSliderFunctionality(); //отработает старте скрипта
   window.addEventListener('resize', function () {
     //отработает при изменении размеров окна
     setSliderFunctionality();
@@ -19,55 +18,99 @@ function sliderCarousel() {
 
         widthWindowTemp = window.getComputedStyle(slidesWindow).width, //сейчас здесь хранится строка.
         widthWindow = +widthWindowTemp.slice(0, widthWindowTemp.length - 2), // а здесь теперь число
-        slidesContainer = item.querySelector('.slider-carousel__container'),//длинный контейнер со слайдами
+        slidesContainer = item.querySelector('.slider-carousel__container'), //длинный контейнер со слайдами
         slides = item.querySelectorAll('.slider-carousel__item'), // коллекция слайдов
         btnLeft = item.querySelector('.slider-carousel__btn-prev'),
         btnRight = item.querySelector('.slider-carousel__btn-next');
 
-      console.log(widthWindow);
 
-      //установка начальных параметров
-      slidesContainer.style.width = 100 * slides.length + '%'; //задать ширину контейнера для слайдов
+      /**============================== */
+      /**установка начальных параметров*/
+      const slideWidth = setSlidesWidth(); // ширина каждого слайда
+      slidesContainer.style.width = slideWidth * slides.length + 'px'; //задать ширину контейнера для слайдов
+      let slidesContainerWidth = slidesContainer.offsetWidth; // ширина контейнера для слайдов, в переменную
 
-      //задать ширину слайдов
-      if (widthWindow <= 500) {
-        console.log('if <500');
-        slides.forEach(item => {
-          item.style.width = `${widthWindow}px`; // задать всем слайдам одинаковую ширину - т.е. ширину контейнера
-        });
-      } else if (widthWindow > 500 && widthWindow <= 800) {
-        console.log('500 > if <=800');
-        slides.forEach(item => {
-          item.style.width = `500px`; // задать всем слайдам одинаковую ширину
-        });
-      } else {
-        console.log('if >800');
-        slides.forEach(item => {
-          item.style.width = `800px`; // задать всем слайдам одинаковую ширину
-        });
-      }
+      /**установить начальное положение slidesContainer*/
+      let offset = setSlidesContainerStart();
 
-      let offset = 0; // смещение slidesContainer(длинный контейнер со слайдами) относительно slidesWindow("окошко", через которое смотрим на  отдельный слайд)
+      /**рассчет крайних точек оффсет*/
+      const offsetLeftEnd = (widthWindow / 2) - (slideWidth / 2); // оффсет для левой стороны
+      const offsetRightEnd = slidesContainerWidth - widthWindow + ((widthWindow / 2) - (slideWidth / 2)); //оффсет для правой стороны
 
+      /**=========== */
       /**обработчик вправо*/
       btnRight.addEventListener('click', () => {
-        if (offset == widthWindow * (slides.length - 1)) {
-          offset = 0;
+        if (offset >= offsetRightEnd) { //если офсет в крайней правой позиции, тогда:
+          offset = offsetLeftEnd; // установить офсет в левую позицию
+          slidesContainer.style.transform = `translateX(${offset}px)`; // смещение на величину offset
         } else {
-          offset += widthWindow;
+          if (offset == offsetLeftEnd) { // если оффет в крайней левой позиции, то:
+            offset = -offset; // меняем знак на противоположный
+          }
+
+          offset += slideWidth; // изменяем оффсет
+          slidesContainer.style.transform = `translateX(-${offset}px)`; //смещение на величину offset
         }
-        slidesContainer.style.transform = `translateX(-${offset}px)`; //смещение на величину offset
       });
 
       /**обработчик влево*/
       btnLeft.addEventListener('click', () => {
-        if (offset == 0) {
-          offset = widthWindow * (slides.length - 1);
+        if (offset <= offsetLeftEnd) { // если оффет крайней левой позиции, тогда:
+          offset = offsetRightEnd; // установить оффсет правую позицию
+          slidesContainer.style.transform = `translateX(-${offset}px)`; //смещение на величину offset
         } else {
-          offset -= widthWindow;
+          offset -= slideWidth;
+
+          if (offset < 0) {
+            offset = -offset; // меняем знак на противоположный
+            slidesContainer.style.transform = `translateX(${offset}px)`; //смещение на величину offset
+          } else {
+            slidesContainer.style.transform = `translateX(-${offset}px)`; //смещение на величину offset
+          }
         }
-        slidesContainer.style.transform = `translateX(-${offset}px)`; //смещение на величину offset
       });
+
+      /**=========== */
+      /** Функции*/
+      function setSlidesWidth() {
+        //задать ширину слайдов в зависимости от ширины вьюпорта
+        let slideWidth = 0;
+
+        if (widthWindow <= 500) {
+          console.log('if <500');
+          slides.forEach(item => {
+            item.style.width = `${widthWindow}px`; // задать всем слайдам одинаковую ширину - т.е. ширину контейнера
+          });
+          slideWidth = widthWindow; // эта переменная будет нужна далее, для вычисления ширины slidesContainer
+          console.log(`slideWidth = ${slideWidth}`);
+        } else if (widthWindow > 500 && widthWindow <= 800) {
+          console.log('500 > if <=800');
+          slides.forEach(item => {
+            item.style.width = `500px`; // задать всем слайдам одинаковую ширину
+          });
+          slideWidth = 500; // эта переменная будет нужна далее, для вычисления ширины slidesContainer
+          console.log(`slideWidth = ${slideWidth}`);
+        } else {
+          console.log('if >800');
+          slides.forEach(item => {
+            item.style.width = `800px`; // задать всем слайдам одинаковую ширину
+          });
+          slideWidth = 800; // эта переменная будет нужна далее, для вычисления ширины slidesContainer
+          console.log(`slideWidth = ${slideWidth}`);
+        }
+
+        return slideWidth;
+      }
+
+      function setSlidesContainerStart() {
+        /**установить начальное (среднее) положение slidesContainer*/
+        const offsetStart = (slidesContainerWidth / 2) - (widthWindow / 2);
+        const offset = offsetStart;
+        console.log(`offset: ${offsetStart}`);
+        slidesContainer.style.transform = `translateX(-${offsetStart}px)`; //смещение на величину offsetStart
+
+        return offset;
+      }
     });
   }
 }
